e0c6b4c74fb19833184fc7595d4de56269c327cd
Axewc <axew@ciencias.unam.mx>
2021-10-12 22:17:56 -0500

    Practica 3

--- Ejercicios/MiPagina/js/script1.js
+++ Ejercicios/MiPagina/js/script1.js
@@ -13,5 +13,15 @@ function validarFormulario(evento) {
         alert('La clave no es válida, prueba con una más larga.');
         return;
     }
+    var email = document.getElementById('email').value;
+    if(email.length == 0) {
+        alert('Correo no puede ser vacío');
+        return;
+    }
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3,4})+$/.test(email)){
+        alert("La dirección de email " + email + " es correcta.");
+    } else {
+        alert("La dirección de email es incorrecta.");
+    }    
     this.submit();
 }
